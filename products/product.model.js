b67e9078b570f7a27abbe14b04d2093c69d68c52
Thi Minh Thu Nguyen <minhthu.vh.24@gmail.com>
2020-12-16 17:05:51 +0100

    change model

--- products/product.model.js
+++ products/product.model.js
@@ -5,16 +5,16 @@ module.exports = model;
 function model(sequelize) {
     const attributes = {
         name: { type: DataTypes.STRING, allowNull: false },
-        brand: { type: DataTypes.STRING, allowNull: true },
+        brand: { type: DataTypes.STRING },
         price: { type: DataTypes.INTEGER, allowNull: false },
         currency_unit: { type: DataTypes.STRING, allowNull: false },
         purchase_date: { type: DataTypes.DATE, allowNull: false },
         expiry_date: { type: DataTypes.DATE, allowNull: false },
         user_id: { type: DataTypes.INTEGER, allowNull: false },
-        merchant: {type: DataTypes.STRING, allowNull: true },
-        location: { type: DataTypes.STRING, allowNull: true },
-        website: { type: DataTypes.STRING, allowNull: true },
-        phone_number: { type: DataTypes.INTEGER, allowNull: true }
+        merchant: {type: DataTypes.STRING},
+        location: { type: DataTypes.STRING },
+        website: { type: DataTypes.STRING },
+        phone_number: { type: DataTypes.INTEGER }
     };
 
     return sequelize.define('Product', attributes);
